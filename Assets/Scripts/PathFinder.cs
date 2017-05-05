d4f454b30237110e533f5c90d6889b909b2a3634
Lucas Soares <lsoares267@gmail.com>
2017-05-05 10:20:48 -0300

    Upando projeto na aula

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 public static class PathFinder
 {
-	public static List<Grid.Position> FindPath( Tile[,] tiles, Grid.Position fromPosition, Grid.Position toPosition )
-	{
-		var path = new List<Grid.Position>();
-		path.Add( fromPosition );
+    public static List<Grid.Position> FindPath(Tile[,] tiles, Grid.Position fromPosition, Grid.Position toPosition)
+    {
+        var path = new List<Grid.Position>();
+        
+        path.Add( fromPosition );
 		path.Add( toPosition );
-
 		return path;
 	}
 }
