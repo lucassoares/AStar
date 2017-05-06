bcee519be0d61efe612d28a166198e7af09e1d07
Lucas Soares <lsoares267@gmail.com>
2017-05-06 20:41:30 -0300

    Corrigindo algoritmo e melhorando gitignore

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -6,8 +6,19 @@ public static class PathFinder
     public static List<Grid.Position> FindPath(Tile[,] tiles, Grid.Position fromPosition, Grid.Position toPosition)
     {
         var path = new List<Grid.Position>();
-        
-        path.Add( fromPosition );
+        path.Add(fromPosition);
+        while(fromPosition.x != toPosition.x || fromPosition.y != toPosition.y)
+        {
+            if (fromPosition.x < toPosition.x) fromPosition.x += 1;
+
+            else if (fromPosition.x > toPosition.x) fromPosition.x -= 1;
+
+            if (fromPosition.y < toPosition.y) fromPosition.y += 1;
+
+            else if (fromPosition.y > toPosition.y) fromPosition.y -= 1;
+
+            path.Add(fromPosition);
+        }
 		path.Add( toPosition );
 		return path;
 	}
