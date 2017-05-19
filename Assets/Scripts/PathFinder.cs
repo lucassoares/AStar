249d9d693e3b7f577211702117d66b1d0e0a8e58
Lucas Soares <lsoares267@gmail.com>
2017-05-19 10:11:24 -0300

    Agora sim!

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -5,21 +5,71 @@ public static class PathFinder
 {
     public static List<Grid.Position> FindPath(Tile[,] tiles, Grid.Position fromPosition, Grid.Position toPosition)
     {
+        PathNode node = BreadthFirstSearch(tiles, fromPosition, toPosition);
         var path = new List<Grid.Position>();
-        path.Add(fromPosition);
-        while(fromPosition.x != toPosition.x || fromPosition.y != toPosition.y)
-        {
-            if (fromPosition.x < toPosition.x) fromPosition.x += 1;
 
-            else if (fromPosition.x > toPosition.x) fromPosition.x -= 1;
-
-            if (fromPosition.y < toPosition.y) fromPosition.y += 1;
-
-            else if (fromPosition.y > toPosition.y) fromPosition.y -= 1;
-
-            path.Add(fromPosition);
-        }
-		path.Add( toPosition );
-		return path;
+        path.Reverse();
+        return path;
 	}
+
+    public class PathNode
+    {
+        public Grid.Position position;
+        public PathNode parent;
+
+        public PathNode(Grid.Position position, PathNode parent)
+        {
+            this.position = position;
+            this.parent = parent;
+        }
+    }
+
+    private static PathNode BreadthFirstSearch(Tile[,] tiles, Grid.Position fromPosition, Grid.Position toPosition)
+    {
+        HashSet<PathNode> nodes = new HashSet<PathNode>();
+        var queue = new Queue<PathNode>();
+
+        var root = new PathNode(fromPosition, null);
+
+        nodes.Add(root);
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            PathNode node = queue.Dequeue();
+            if (node.position.x == toPosition.x && node.position.y == toPosition.y)
+            {
+                Debug.Log("ACHEI O CARA");
+                return node;
+            }
+            else
+            {
+                TryEnqueueNode(tiles, queue, nodes, node, new Grid.Position(node.position.x + 1, node.position.y));
+                TryEnqueueNode(tiles, queue, nodes, node, new Grid.Position(node.position.x - 1, node.position.y));
+                TryEnqueueNode(tiles, queue, nodes, node, new Grid.Position(node.position.x, node.position.y + 1));
+                TryEnqueueNode(tiles, queue, nodes, node, new Grid.Position(node.position.x, node.position.y - 1));
+            }
+        }
+        return null;
+    }
+
+    private static void TryEnqueueNode(Tile[,] tiles, Queue<PathNode> queue, HashSet<PathNode> nodes, PathNode currentNode, Grid.Position position)
+    {
+        int width = tiles.GetLength(0);
+        int height = tiles.GetLength(1);
+
+
+        if(position.x > width || position.x < 0 || position.y > height || position.y < 0)
+        {
+            Debug.Log("Não está na grade");
+            return;
+        }
+        else
+        {
+            var node = new PathNode(position, currentNode);
+            nodes.Add(node);
+            queue.Enqueue(node);
+
+        }
+    }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -35,6 +35,6 @@ public class Tile : MonoBehaviour
 
 	private void OnMouseDown()
 	{
-		grid.MoveTo( position );
-	}
+        if (!isWall) grid.MoveTo(position);
+    }
 }
